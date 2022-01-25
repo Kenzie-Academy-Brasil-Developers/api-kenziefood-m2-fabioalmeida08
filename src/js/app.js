3c72b3120167f18ce58e83da39d75a03bd94554c
Fábio <fabioalmeida0896@gmail.com>
2022-01-25 17:48:48 -0300

    event listeners add

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,20 @@
 import { Carrinho } from "./Carrinho.js";
 import { Produtos , db} from "./Produto.js";
+const spacoCards = document.querySelector('.espacoCards')
+const campoBusca = document.querySelector('input')
 
 Produtos.criarPaginaInicial()
-Carrinho.addProduto(1)
+Carrinho.addProduto(3)
+
+campoBusca.addEventListener('keyup' , () => {
+    let value = campoBusca.value
+    Produtos.Busca(value)
+})
+
+spacoCards.addEventListener('click' , (e) => {
+    let tar = e.target
+    if(tar.tagName === 'BUTTON') {
+        let id = Number(tar.getAttribute('prodid'))
+        Carrinho.addProduto(id)
+    }
+})
