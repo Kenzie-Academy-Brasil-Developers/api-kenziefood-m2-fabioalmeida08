86032eabc2d988215ad9922af20095b2ba40ed80
Vinicius <vinicupolillo@yahoo.com.br>
2022-01-26 10:05:19 -0300

    Produtos do carrinho adicionados a um array. Quantidade de produtos e preco total feito

--- src/js/Carrinho.js
+++ src/js/Carrinho.js
@@ -1,5 +1,5 @@
 import {Produtos , db} from './Produto.js'
-
+const arrProduto = [];
 
 class Carrinho {
     
@@ -9,40 +9,61 @@ class Carrinho {
 		)
 		let produtos = await prod.json()
 		let final = await produtos
-        const produto = final.find(item => item.id === id)
-
+        arrProduto.push( final.find(item => item.id === id) )
         const carrinhoCard =  document.createElement('div')
-        carrinhoCard.classList.add('carrinho-card')
-        
-        const prodImg = document.createElement('img')
-        prodImg.src = produto.imagem
-        carrinhoCard.appendChild(prodImg)
-        
-        const carrinhoCardText = document.createElement('div')
-        const h2 = document.createElement('h2')
-        h2.innerHTML = produto.nome
-        carrinhoCardText.appendChild(h2)
+        arrProduto.forEach((produto) => {    
+            carrinhoCard.innerHTML = '';
+            carrinhoCard.classList.add('carrinho-card')
+            
+            const prodImg = document.createElement('img')
+            prodImg.src = produto.imagem
+            carrinhoCard.appendChild(prodImg)
+            
+            const carrinhoCardText = document.createElement('div')
+            const h2 = document.createElement('h2')
+            h2.innerHTML = produto.nome
+            carrinhoCardText.appendChild(h2)
 
-        const p = document.createElement('p')
-        p.innerHTML = produto.categoria
-        carrinhoCardText.appendChild(p)
-        
-        const h3 = document.createElement('h3')
-        h3.innerHTML = `R$ ${produto.preco.toFixed(2)}`
-        carrinhoCardText.appendChild(h3)
+            const p = document.createElement('p')
+            p.innerHTML = produto.categoria
+            carrinhoCardText.appendChild(p)
+            
+            const h3 = document.createElement('h3')
+            h3.innerHTML = `R$ ${produto.preco.toFixed(2)}`
+            carrinhoCardText.appendChild(h3)
 
-        const lixo = document.createElement('button')
-        lixo.setAttribute('lixoid', produto.id)
-        lixo.innerHTML = '🗑️'
-        carrinhoCard.appendChild(lixo)
+            const lixo = document.createElement('button')
+            lixo.setAttribute('lixoid', produto.id)
+            lixo.innerHTML = '🗑️'
+            carrinhoCard.appendChild(lixo)
 
-        carrinhoCard.appendChild(carrinhoCardText) 
-        const cbody = document.querySelector('.carrinho-body')
-        cbody.appendChild(carrinhoCard)
+            carrinhoCard.appendChild(carrinhoCardText) 
+            const cbody = document.querySelector('.carrinho-body')
+            cbody.appendChild(carrinhoCard)
+        })
+        this.calculoPreco(arrProduto,carrinhoCard)
     }
 
-    static calculoPreco() {
+    static calculoPreco(produtos,carrinhoCard) {
+        let total = 0;
+        const carrinhoFooter = document.createElement('div');
+        produtos.forEach((produto) => {
+            total = total + produto.preco;
+            carrinhoFooter.innerHTML = '';
+            carrinhoFooter.classList.add('carrinho-footer');
 
+            const carrinhoQuant = document.createElement('div');
+            const p = document.createElement('p');
+            p.innerHTML = `Quantidade de Produtos ${produtos.length}`;
+            carrinhoQuant.appendChild(p);
+
+            const pTotal = document.createElement('p');
+            pTotal.innerHTML = `Valor Total R$ ${total}`
+            carrinhoQuant.appendChild(pTotal);
+
+            carrinhoFooter.appendChild(carrinhoQuant);
+            carrinhoCard.appendChild(carrinhoFooter);
+    })
     }
 
     static removerProduto() {
