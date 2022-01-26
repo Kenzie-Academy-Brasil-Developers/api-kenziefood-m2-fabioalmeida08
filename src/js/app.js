fb5fcd749c8df7f520f3b7dfeb8a37f96a4d4934
Fábio <fabioalmeida0896@gmail.com>
2022-01-26 08:41:07 -0300

    events listeners add para navbar

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,28 @@ import { Carrinho } from "./Carrinho.js";
 import { Produtos } from "./Produto.js";
 const spacoCards = document.querySelector('.espacoCards')
 const campoBusca = document.querySelector('input')
+const todos = document.querySelector('#filtro-todos')
+const panificadora = document.querySelector('#filtro-panificadora')
+const frutas = document.querySelector('#filtro-frutas')
+const bebidas = document.querySelector('#filtro-bebidas')
 
 Produtos.criarPaginaInicial()
-Carrinho.addProduto(3)
 
+todos.addEventListener('click' , () => {
+    Produtos.criarPaginaInicial()
+})
+
+panificadora.addEventListener('click' , () => {
+    Produtos.filtroCat('Panificadora')
+})
+
+frutas.addEventListener('click' , () => {
+    Produtos.filtroCat('Frutas')
+})
+
+bebidas.addEventListener('click' , () => {
+    Produtos.filtroCat('Bebidas')
+})
 campoBusca.addEventListener('keyup' , () => {
     let value = campoBusca.value
     Produtos.Busca(value)
