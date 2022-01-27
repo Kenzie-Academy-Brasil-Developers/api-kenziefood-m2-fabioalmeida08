cca8a310c43a983affedc35ff89958ce9cbcf8c0
Fábio <fabioalmeida0896@gmail.com>
2022-01-27 09:15:41 -0300

    db criada

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,47 @@
-import { Carrinho } from "./Carrinho.js";
-import { Produtos } from "./Produto.js";
+import { Carrinho } from './Carrinho.js'
+import { Produtos } from './Produto.js'
 const spacoCards = document.querySelector('.espacoCards')
 const campoBusca = document.querySelector('input')
 const todos = document.querySelector('#filtro-todos')
 const panificadora = document.querySelector('#filtro-panificadora')
 const frutas = document.querySelector('#filtro-frutas')
 const bebidas = document.querySelector('#filtro-bebidas')
-
 const cbody = document.querySelector('.carrinho-body')
-
 
 Produtos.criarPaginaInicial()
 
-todos.addEventListener('click' , () => {
-    Produtos.criarPaginaInicial()
+todos.addEventListener('click', () => {
+	Produtos.criarPaginaInicial()
 })
 
-panificadora.addEventListener('click' , () => {
-    Produtos.filtroCat('Panificadora')
+panificadora.addEventListener('click', () => {
+	Produtos.filtroCat('Panificadora')
 })
 
-frutas.addEventListener('click' , () => {
-    Produtos.filtroCat('Frutas')
+frutas.addEventListener('click', () => {
+	Produtos.filtroCat('Frutas')
 })
 
-bebidas.addEventListener('click' , () => {
-    Produtos.filtroCat('Bebidas')
+bebidas.addEventListener('click', () => {
+	Produtos.filtroCat('Bebidas')
 })
-campoBusca.addEventListener('keyup' , () => {
-    let value = campoBusca.value
-    Produtos.Busca(value)
-})
-
-spacoCards.addEventListener('click' , (e) => {
-    let tar = e.target
-    if(tar.tagName === 'BUTTON') {
-        let id = Number(tar.getAttribute('prodid'))
-        Carrinho.addProduto(id)
-    }
+campoBusca.addEventListener('keyup', () => {
+	let value = campoBusca.value
+	Produtos.Busca(value)
 })
 
-cbody.addEventListener('click' , (e) =>{
-    let tar = e.target
-    if(tar.tagName === 'BUTTON') {
-        let id = Number(tar.getAttribute('lixoid'))
-        Carrinho.removerProduto(id)
-    }
+spacoCards.addEventListener('click', (e) => {
+	let tar = e.target
+	if (tar.tagName === 'BUTTON') {
+		let id = Number(tar.getAttribute('prodid'))
+		Carrinho.addProduto(id)
+	}
+})
+
+cbody.addEventListener('click', (e) => {
+	let tar = e.target
+	if (tar.tagName === 'BUTTON') {
+		let id = Number(tar.getAttribute('lixoid'))
+		Carrinho.removerProduto(id)
+	}
 })
